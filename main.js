b2e38ab026c1a2e8b94e55b1a5850f5e4df54552
Stiven Peter <Peter@Stivens-MBP.fios-router.home>
2017-02-28 20:43:21 -0500

    done

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-var svg = document.getElementByD("vimage");
-var x = -1
-var y = -1
+var svg = document.getElementById("vimage");
+var x = -1;
+var y = -1;
+var NotFirst = false;
 var makeCircle = function(e) {
-    var mouseX = e.offsetX;
-    var mouseY = e.offsetY;
-    var c = document.createElementNS("https://wwww.w3.org/2000/svg","line");
-    c.setAttribute("cx", mouseX);
-    c.setAttribute("cy", mouseY);
-    c.setAttribute("r", "20") 
-    c.setAttribute("fill","yellow");
-    svg.appendChild(c);
+	var mouseX = e.offsetX;
+	var mouseY = e.offsetY;
+	var circle = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    var l = document.createElementNS("http://www.w3.org/2000/svg","line");
+    circle.setAttribute("cx", mouseX);
+    circle.setAttribute("cy", mouseY);
+    circle.setAttribute("r", "20");
+    circle.setAttribute("fill","red");
+    svg.appendChild(circle);
+    if (NotFirst) {
+    	l.setAttribute("x1",x);
+    	l.setAttribute("y1",y);
+    	l.setAttribute("x2",mouseX);
+    	l.setAttribute("y2",mouseY);
+    	l.setAttribute("stroke","black");
+    	svg.appendChild(l);
+    }
+    x = mouseX;
+    y = mouseY;
+    NotFirst = true;
+
 };
 var clearSVG = function() {
-    svg.empty();
+    while(svg.hasChildNodes()) {
+    	svg.removeChild(svg.lastChild);
+    }
+	NotFirst = false;
  };
 
-c.addEventListener("click", makeCircle);
-document.getElementById("clear").addEventListener("click", clearSVG);
+svg.addEventListener("click", makeCircle);
+var b = document.getElementById("clear");
+b.addEventListener("click",clearSVG);
